8e2e36c7d070c21e18e0b7d614e75529133908a4
adeleong <aderso.deleon@gmail.com>
2015-03-11 05:46:29 -0400

    2do commit lesson 4A

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,14 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        public  static final String COLUMN_CITY_NAME = "city_name";
+
+        public  static final String COLUMN_COORD_LAT = "cood_lat";
+
+        public  static final String COLUMN_COORD_LONG = "cood_long";
+
     }
 
     /* Inner class that defines the table contents of the weather table */
